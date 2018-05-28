f392e81bed4e3b0c283a5a03c21f8daf96010aec
Funce <funce.973@gmail.com>
2018-05-29 04:34:59 +1200

    0435 am

--- dashboard.php
+++ dashboard.php
@@ -11,6 +11,24 @@
 	{
 		header("Location:index.php");
 	}
+	
+	//Check if denied
+	if(isset($_POST['deny_x']))
+	{
+		$img_id = mysqli_real_escape_string($link, $_POST['img_id']);
+		$query = "UPDATE tbl_img SET img_status = '1' WHERE img_id = '$img_id'";
+		mysqli_query($link, $query);
+	}
+	elseif(isset($_POST['approve_x']))
+	{//Check if its been approved
+		if($_POST['category'] != "0")
+		{//Check if they've selected a category
+			$img_id = mysqli_real_escape_string($link, $_POST['img_id']);
+			$img_cat_id = mysqli_real_escape_string($link, $_POST['category']);
+			$query = "UPDATE tbl_img SET img_status = '0', img_cat_id = $img_cat_id WHERE img_id = '$img_id'";
+			mysqli_query($link, $query);
+		}
+	}
 ?>
 
 <!DOCTYPE html>
@@ -41,52 +59,57 @@
 				</div>
   				<div class="col-sm-6 col-sm-pull-6">
 					<div><h2>Photos to be approved</h2></div>
-					<table>
-						<tr>
-							<th>Image</th>
-							<th>Title</th>
-							<th>Credit</th>
-							<th>Uploader</th>
-							<th>Category</th>
-							<th>Actions</th>
-						</tr>
+					<div class="table">
+						<div class="tr" style="font-weight: bold;">
+							<span class="td">Image</span>
+							<span class="td">Title</span>
+							<span class="td">Credit</span>
+							<span class="td">Uploader</span>
+							<span class="td">Category</span>
+							<span class="td">Actions</span>
+						</div>
 <?php
-						$rowsPerPage = 20;
 						$query = "SELECT img_id, img_temp_name, img_title, img_credit, user_name FROM tbl_img JOIN tbl_users ON (user_id=img_user_id) WHERE img_status!='1' AND img_cat_id IS NULL";
-						$pagingLink = getPagingLink($query, $rowsPerPage, "");
-						$result = mysqli_query($link, getPagingQuery($query, $rowsPerPage));
+						$result = mysqli_query($link, $query);
 						if(mysqli_num_rows($result))
 						{
 							while($row = mysqli_fetch_assoc($result))
 							{
-?>								<tr>
-									<input type="hidden" name="img_id" value="<?=$row['img_id'];?>" />
-									<td class="thumbnail"><img style="width: 70px;" src="img/<?=$row['img_temp_name'];?>" /></td>
-									<td><?=$row['img_title'];?></td>
-									<td><?=$row['img_credit'];?></td>
-									<td><?=$row['user_name'];?></td>
-									<td><select>
-										<option value='0'>Select or Decline</option>
+?>
+								<form class="tr" action="<?=$_SERVER["REQUEST_URI"];?>" method="post">
+								
+									
+										<input type="hidden" name="img_id" value="<?=$row['img_id'];?>" />
+										<span class="td"><img style="width: 70px;" src="img/<?=$row['img_temp_name'];?>" /></span>
+										<span class="td"><?=$row['img_title'];?></span>
+										<span class="td"><?=$row['img_credit'];?></span>
+										<span class="td"><?=$row['user_name'];?></span>
+										<span class="td"><select name="category">
+											<option value='0'>Select or Decline</option>
 <?php
-										$query = "SELECT * FROM tbl_category";
-										$interest = mysqli_query($link, $query);
-							 			if(mysqli_num_rows($interest))
-										{
-											while($cats = mysqli_fetch_assoc($interest))
+											$query = "SELECT * FROM tbl_category";
+											$interest = mysqli_query($link, $query);
+											if(mysqli_num_rows($interest))
 											{
-	?>
-												<option value=<?=$cats['cat_id'];?>><?=$cats['cat_name'];?></option>
-	<?php		
+												while($cats = mysqli_fetch_assoc($interest))
+												{
+?>
+													<option value=<?=$cats['cat_id'];?>><?=$cats['cat_name'];?></option>
+<?php		
+												}
 											}
-										}
-							 ?>
-										</select></td>
-								</tr>
+								 ?>
+											</select></span>
+										<span class="td"><input style="width: 20px;" type="image" src="/img/check.png" name="approve" />
+														 <input style="width: 20px;" type="image" src="/img/cross.png" name="deny" /></span>
+									
+						
+								</form>
 <?php
 							}
 						}
 ?>
-					</table>
+					</div>
 				</div>
 			</div>
 		</div>
--- sidebar.php
+++ sidebar.php
@@ -10,7 +10,16 @@
 				$cat_name = mysqli_real_escape_string($link, $_POST['add-category']);
 				$query = "INSERT INTO tbl_category (cat_name) VALUES ('$cat_name')";
 				mysqli_query($link, $query);
-			}	
+			}
+			if(isset($_POST['remove_x']))
+			{
+				echo "removing";
+				$cat_id = mysqli_real_escape_string($link, $_POST['cat_id']);
+				$query = "UPDATE tbl_img SET img_cat_id=NULL WHERE img_cat_id = '$cat_id'";
+				mysqli_query($link, $query);
+				$query = "DELETE FROM tbl_category where cat_id='$cat_id'";
+				mysqli_query($link, $query);
+			}
 ?>
 <?php
 			$query = "SELECT * FROM tbl_category";
@@ -19,8 +28,11 @@
 			{
 				while ($row = mysqli_fetch_assoc($result))
 				{
-?>
-					<a href="<?=$_SERVER["SCRIPT_NAME"];?>?catid=<?=$row['cat_id'];?>"><div class="sidebar-item"><?=$row['cat_name'];?></div></a>
+?>					
+					<form action="<?=$_SERVER["REQUEST_URI"];?>" method="post">
+					<input type="hidden" name="cat_id" value="<?=$row['cat_id'];?>">
+					<a href="<?=$_SERVER["SCRIPT_NAME"];?>?catid=<?=$row['cat_id'];?>"><div class="sidebar-item"><?=$row['cat_name'];?>&nbsp;<input type="image" style="width:15px;" name="remove" src="/img/cross.png"/></div></a>
+					</form>
 <?php	
 				}
 			}
@@ -34,18 +46,33 @@
 				</table>
 			</form>
 <?php	}
-	}
-	else{
-		$query = "SELECT * FROM tbl_category";
-		$result = mysqli_query($link, $query);
-		if(mysqli_num_rows($result))
-		{
-			while ($row = mysqli_fetch_assoc($result))
-			{
+		else
+	{
+			$query = "SELECT * FROM tbl_category";
+				$result = mysqli_query($link, $query);
+				if(mysqli_num_rows($result))
+				{
+					while ($row = mysqli_fetch_assoc($result))
+					{
 ?>
-				<a href="<?=$_SERVER["SCRIPT_NAME"];?>?catid=<?=$row['cat_id'];?>"><div class="sidebar-item"><?=$row['cat_name'];?></div></a>
+						<a href="<?=$_SERVER["SCRIPT_NAME"];?>?catid=<?=$row['cat_id'];?>"><div class="sidebar-item"><?=$row['cat_name'];?></div></a>
 <?php	
-			}
+					}
+				}
 		}
+	}
+	else
+	{
+		$query = "SELECT * FROM tbl_category";
+			$result = mysqli_query($link, $query);
+			if(mysqli_num_rows($result))
+			{
+				while ($row = mysqli_fetch_assoc($result))
+				{
+?>
+					<a href="<?=$_SERVER["SCRIPT_NAME"];?>?catid=<?=$row['cat_id'];?>"><div class="sidebar-item"><?=$row['cat_name'];?></div></a>
+<?php	
+				}
+			}
 	}
 ?>
